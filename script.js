b741fdb301fbec07c1971da3f83c7d1425d58bd4
jgulian <josephdgulian@gmail.com>
2020-10-01 19:00:31 -0400

    CAVE UPDATE!!!!

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ function working_days(fromDate, toDate) {
         toD = new Date(toDate.getTime()),
         numOfWorkingDays = 1;
 
-    // reset time portion
     frD.setHours(0, 0, 0, 0);
     toD.setHours(0, 0, 0, 0);
 
@@ -92,5 +91,34 @@ function tomorrow() {
     });
 }
 
-update_letter(new Date());
-document.getElementById("date").innerHTML = format_date(new Date());
+function firstLessThan(arr, val) {
+    let count = 0;
+    for (let day of arr) {
+        if (val < new Date(day))
+            return count;
+        count++;
+    }
+    return count;
+}
+
+function datediff(first, second) {
+    return Math.round((second-first)/(1000*60*60*24));
+}
+
+async function otherStuff(today) {
+    resposnse = await fetch("/days_off.json")
+    days_off = await resposnse.json();
+
+    const the_end = new Date('June 15, 2021');
+    const the_bad = new Date('December 15, 2020');
+    const days_till_the_end = 180 - (working_days(new Date("9/8/20"), today) - firstLessThan(days_off, today));
+    document.getElementById("other").setAttribute('tbd', datediff(today, the_bad) + 1);
+    document.getElementById("other").setAttribute('end', days_till_the_end);
+}
+
+const today = new Date()
+
+update_letter(today);
+document.getElementById("date").innerHTML = format_date(today);
+
+otherStuff(today)
